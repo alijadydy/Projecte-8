04b0b89d9b3f42c6943223b2301a6eded9a770d7
alijadydy <ael1@iesjulioantonio.cat>
2025-05-22 11:14:02 +0200

    Delete P6_RA2_Funcions i Moduls directory

--- P6_RA2_Funcions i Moduls/2.5 EXERCICIS PER CREAR I USAR MÒDULS/utilitats/__init__.py
+++
@@ -1,5 +0,0 @@
-# -------------------------------------------------------------
-# Nom de l'alumne: Ali
-# Data: 05/05/2025
-# Mòdul: Projecte 6 - RA2 - EXERCICIS AMB MÒDULS EXTERNS O ORGANITZACIÓ DE PROJECTES
-# -------------------------------------------------------------
--- P6_RA2_Funcions i Moduls/P6_RA1_5_Ali.py
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------------------------
-# Nom de l'alumne: Ali
-# Data: 05/05/2025
-# Mòdul: Projecte 6 - RA2 - Funcions i Mòduls
-# -------------------------------------------------------------
-
-
-def saluda_nom(nom="amic"):
-    print(f"Hola, {nom}")
